322fb0c0ee080de539650ffbccbc67f76aa7cde0
sergiovictorprojects <sergiocassimirosilva21@gmail.com>
2026-09-14 19:50:22 +0000

    Inclusão de arquivos para apresentação, atualização nos repositórios, criação de painel iterativo de acordo com os relacionamentos no Neo4j no index

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
-# DIO-project-SSR
-# Sistema de Busca por Recomendação
+# DIO-project-SSR v1.0
+Sistema de Busca por Recomendação (SSR)
 
-# Visão geral
-Este projeto está sendo desenvolvido como uma solução inicial para busca e recomendação de conteúdos, com foco em organizar e recuperar informações relevantes de usuários de forma eficiente.
+![Tela01](img/png/database/image.png)
 
-# Objetivo
+## Visão geral
+Este projeto será desenvolvido como uma solução inicial para busca e recomendação de conteúdos, com foco em organizar e recuperar informações relevantes de usuários de forma eficiente.
+
+## Objetivo
 Inicialmente é documentar todo o processo de desenvolvimento do projeto. Sua finalização será pelo Neo4j com visualização dos grafos correlacionados de acordo com os conteúdos e os usuários, com base em critérios de busca e afinidade, utilizando uma estrutura simples e escalável para evolução futura.
 
-# Status atual
+## Status atual
 Até o momento, o repositório foi inicializado com a estrutura base do projeto e a definição do nome e objetivo principal.
 A base de dados foi criada apenas com informações iniciais. 
 
-# Próximos passos
-- Incluir eventos na base de dados (simples);
-- Implementar uma camada de busca;
-- Estruturar regras de recomendação;
-- Documentar a evolução do projeto.
+
+## Conteúdo
+Base de dados utilizada no projeto:
+
+![Tela02](img/png/database/image2.png)
+![Tela03](img/png/database/image3.png)
+![Tela04](img/png/database/image4.png)
--- dados/app.py
+++ dados/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import pandas as pd
+import json
 
 app = Flask(__name__)
 
@@ -7,10 +8,25 @@ app = Flask(__name__)
 def home():
     titulo_projeto = "SSR (Search System for Recommendations)"
     tecnologias = ["Python", "Neo4j", "Flask", "HTML", "GitHub Codespaces"]
-    tabela1 = pd.read_excel("dados/base/usuarios_filmes_series.xlsx")
-    tabela_html = tabela1.to_html(classes="user-table", index=False)
+    
+    tb_usuarios = pd.read_excel("dados/base/usuarios_filmes_series.xlsx", sheet_name="Usuários")
+    html_usuarios = tb_usuarios.to_html(classes="user-table", index=False)
+    
+    tb_seriesfilmes = pd.read_excel("dados/base/usuarios_filmes_series.xlsx", sheet_name="Filmes e Séries")
+    tb_seriesfilmes = tb_seriesfilmes.drop_duplicates(subset=['Tipo'])
+    html_seriesfilmes = tb_seriesfilmes.to_html(classes="seriesfilmes-table", index=False)
 
-    return render_template("index.html", titulo=titulo_projeto, itens=tecnologias, relacaoent=tabela_html)
+    tb_eventos = pd.read_excel("dados/base/usuarios_filmes_series.xlsx", sheet_name="Eventos")
+    tb_eventos = tb_eventos.drop_duplicates(subset=['Usuário'])
+    html_eventos = tb_eventos.to_html(classes="eventstable", index=False)
+
+    return render_template("index.html", 
+                           titulo=titulo_projeto, 
+                           itens=tecnologias, 
+                           usertable=html_usuarios, 
+                           seriesfilmes=html_seriesfilmes,
+                           eventstable=html_eventos,)
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", debug=True, port=8080)
